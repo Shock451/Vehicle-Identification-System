3719ee202dac6b6a047f4a52829066fd1d57945b
unknown <ayobami.oladosu@gmail.com>
2019-06-06 02:50:13 +0100

    Yello

---
+++ public/javascripts/script.js
@@ -0,0 +1,6 @@
+var navBarToggle = document.getElementById('hamburger');
+var navMenu = document.getElementById('nav-menu');
+
+navBarToggle.addEventListener('click', function () {
+    navMenu.classList.toggle('active');
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* org signup page. */
+router.get('/org/signup', function (req, res, next) {
+  res.render('org-signup', { title: 'Express' });
+});
+
+/* Individual signup page. */
+router.get('/individual/signup', function (req, res, next) {
+  res.render('ind-signup', { title: 'Express' });
+});
+
+/* Individual signin page. */
+router.get('/individual/signin', function (req, res, next) {
+  res.render('ind-signin', { title: 'Express' });
+});
+
+/* Company signin page. */
+router.get('/org/signin', function (req, res, next) {
+  res.render('org-signin', { title: 'Express' });
+});
+
 module.exports = router;
